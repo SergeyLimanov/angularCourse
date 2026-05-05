8cf50cd430d36687c88b604b775a0e379500fd7f
slimanov <imajing2010@yandex.ru>
2026-05-05 17:57:26 +0400

    исправление запуска

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-import {CoreModule} from "./modules/core/core.module";
-import {CoursesModule} from "./modules/courses/courses.module";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  imports: [
-    CoreModule,
-    CoursesModule
-  ],
-  standalone: true
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-bs';

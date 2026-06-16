d5a683d596e0f364a1ef72d91dbe750e641a1d8d
slimanov <slimanov@diasoft.ru>
2026-06-16 16:38:38 +0300

    5. Роутинг запуск2

--- my-app/src/app/modules/courses/courses-routing.module.ts
+++ my-app/src/app/modules/courses/courses-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { CoursesComponent } from './courses.component';
+import { CourseAddComponent } from './components/course-add/course-add.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: CoursesComponent
+  },
+  {
+    path: 'new',
+    component: CourseAddComponent
+  },
+  {
+    path: ':id',
+    component: CourseAddComponent
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class CoursesRoutingModule { }

9a007d8c89120e23583207653930e0e2c242402e
slimanov <slimanov@diasoft.ru>
2026-06-16 15:15:14 +0300

    5. Роутинг задания: Исправьте/обновите ссылки на странице курсов. (в процессе)

--- my-app/src/app/modules/courses/components/course-add/course-add.component.ts
+++ my-app/src/app/modules/courses/components/course-add/course-add.component.ts
@@ -1,7 +1,8 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Course } from "../../../../interface/course.interface";
 import { CoursesService } from "../../../../services/courses.service";
-import {MenuItem} from "primeng/api";
+import { MenuItem } from "primeng/api";
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-course-add',
@@ -16,40 +17,60 @@ export class CourseAddComponent implements OnInit {
   description: string = '';
   duration: number = 0;
   creationDate: Date = new Date();
-   newCourse: Course = {
+  newCourse: Course = {
     id: 0,
-    title: "",
+    title: '',
     description: this.description,
     duration: this.duration,
     creationDate: this.creationDate,
     topRated: false,
-   };
+  };
   coursesMenu: MenuItem[] = [];
 
-  constructor(private coursesService: CoursesService) {}
+  constructor(
+    private coursesService: CoursesService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
-    console.log('courseToEdit:', this.courseToEdit);
-    this.coursesMenu = [
-      { label: 'Курсы',  },
+   ngOnInit(): void {
+     console.log('CourseAddComponent ngOnInit');
+
+     this.coursesMenu = [
+      { label: 'Курсы' },
     ];
+
+    console.log('ADD COURSE')
+
+    const courseId = this.route.snapshot.paramMap.get('id');
+    console.log('Course ID from route:', courseId);
+
+    if (courseId) {
+      const course = this.coursesService.getCourseById(Number(courseId));
+      console.log('Course fetched by ID:', course);
+      if (course) {
+        this.newCourse = { ...course };
+        this.description = this.newCourse.description;
+        this.duration = this.newCourse.duration;
+        this.creationDate = this.newCourse.creationDate;
+      }
+    }
   }
 
   // Логика сохранения курса
   save(): void {
-    if (this.courseToEdit) {
-      // Логика для обновления существующего курса
+    if (this.newCourse.id) {
+      // Обновление существующего курса
+      this.coursesService.update(this.newCourse);
     } else {
-      // Логика для добавления нового курса
+      // Добавление нового курса
+      this.coursesService.create(this.newCourse);
     }
-    console.log('Course saved:', this.newCourse);
-    console.log('Курс сохранён с продолжительностью:', this.duration);
-    this.formClosed.emit(); // Закрытие формы после сохранения
-
+    this.router.navigateByUrl("/courses");
   }
 
   cancel(): void {
     this.formClosed.emit(); // Закрытие формы без сохранения
+    this.router.navigate(['/courses']); // Перенаправление на страницу курсов
   }
-
 }
